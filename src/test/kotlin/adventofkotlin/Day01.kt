c433772de66602c415940d2c91699b7090df87c3
Tillmann Heigel <tillmann.heigel@idealo.de>
2020-12-04 11:15:40 +0100

    Restructure to be able to test it

--- src/test/kotlin/adventofkotlin/Day01.kt
+++ src/test/kotlin/adventofkotlin/Day01.kt
@@ -1,33 +1,70 @@
 package adventofkotlin
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class Day01 : Advent2020() {
 
-    private lateinit var integers: List<Int>
+    @Test
+    fun test01() {
+        val integers = readLines("testinput01")!!.map(Integer::parseInt)
 
-    @BeforeEach
-    fun setup() {
-        integers = readLines("input01")!!.map(Integer::parseInt)
+        val result = findTuple(integers)
+
+        assertEquals(result, 514579)
+    }
+
+    @Test
+    fun test02() {
+        val integers = readLines("testinput01")!!.map(Integer::parseInt)
+
+        val result = findTriple(integers)
+
+        assertEquals(result, 241861950)
+    }
+
+    @Test
+    fun run01() {
+        val integers = readLines("input01")!!.map(Integer::parseInt)
+
+        val result = findTuple(integers)
+
+        assertEquals(result, 658899)
     }
 
     @Test
     fun run02() {
-        integers.any { a ->
-            integers
-                .any { b ->
-                    integers
-                        .any { c -> match(a, b, c) }
-                }
-        }
+        val integers = readLines("input01")!!.map(Integer::parseInt)
+
+        val result = findTriple(integers)
+
+        assertEquals(result, 155806250)
     }
 
-    private fun match(a: Int, b: Int, c: Int): Boolean {
-        val result = a + b + c == 2020
-        if (result) {
-            print(a * b * c)
+    private fun findTuple(integers: List<Int>): Int {
+        var result = 0
+        integers.any { a ->
+            integers.any { b ->
+                    val check = a + b  == 2020
+                    if (check) result = a * b
+                    check
+            }
         }
         return result
     }
+
+    private fun findTriple(integers: List<Int>): Int {
+        var result = 0
+        integers.any { a ->
+            integers.any { b ->
+                integers.any { c ->
+                    val check = a + b + c == 2020
+                    if (check) result = a * b * c
+                    check
+                }
+            }
+        }
+        return result
+    }
+
 }
