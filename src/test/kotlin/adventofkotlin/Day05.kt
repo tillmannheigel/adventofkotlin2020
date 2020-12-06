689eb8b3203312363189ce7f055d1f132e41209d
Tillmann Heigel <tillmann.heigel@idealo.de>
2020-12-06 17:15:59 +0100

    Day 05 with new variant b

--- src/test/kotlin/adventofkotlin/Day05.kt
+++ src/test/kotlin/adventofkotlin/Day05.kt
@@ -22,17 +22,35 @@ class Day05 : Advent2020() {
 
     @Test
     fun run02() {
-
         val seatIds = readLines("input05")!!
             .map { seatId(it) }
             .sorted()
 
-        val complete = seatIds.first()..seatIds.last()
-
-        val missing = complete.filter { i -> !seatIds.contains(i) }
+        val missing = (seatIds.first()..seatIds.last()).filter { !seatIds.contains(it) }
 
         println(missing)
+    }
 
+
+    @Test
+    fun run02_variantB() {
+        val seatIds = readLines("input05")!!
+            .map { it.replace('F', '0') }
+            .map { it.replace('B', '1') }
+            .map { it.replace('L', '0') }
+            .map { it.replace('R', '1') }
+            .map {
+                Pair(
+                    Integer.parseInt(it.subSequence(0, 7).toString(), 2),
+                    Integer.parseInt(it.subSequence(7, 10).toString(), 2)
+                )
+            }
+            .map { it.first * 8 + it.second }
+            .sorted()
+
+        val missing = (seatIds.first()..seatIds.last()).filter { !seatIds.contains(it) }
+
+        println(missing)
     }
 
     private fun seatId(it: String): Int {
